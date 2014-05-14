49d2c519eb3a539acbb9b237e28388b40df8a519
vagrant <vagrant@dockernetwork>
2014-05-14 02:28:33 +0000

    clean up & openjdk-7 for aurora testing

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ./docker-mesos/mesos-slave && docker build -t mesos-slave .
+cd ./docker-mesos/mesos-master-ssh && docker build -t mesos-master-ssh .
 cd ./docker-mesos/mesos-slave-ssh && docker build -t mesos-slave-ssh .
 cd ./master-with-marathon && docker build -t mesos .
--- marathon/src/main/scala/mesosphere/marathon/api/v2/json/EnrichedTask.scala
+++
@@ -1,6 +0,0 @@
-package mesosphere.marathon.api.v2.json
-
-import mesosphere.marathon.Protos.MarathonTask
-import mesosphere.marathon.health.HealthCheckActor.Health
-
-case class EnrichedTask(appId: String, task: MarathonTask, healthCheckResults : Seq[Option[Health]])
