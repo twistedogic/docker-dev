c6aa6d21c0447d6217b5adbce323db24998d3c66
vagrant <vagrant@dockernetwork>
2014-05-15 11:29:57 +0000

    add consul for monitoring

--- ssh-mesos-cluster.sh
+++ ssh-mesos-cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 location=$(pwd)
-CID=$(docker run -m=104857600 -d -p 5050:5050 -p 8080:8080 -p 9002:22 -p 2181:2181 mesos)
+CID=$(docker run -m=104857600 -d -p 5050:5050 -p 8080:8080 -p 9002:22 -p 2181:2181 -p 8500:8500 mesos-consul)
 MIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID})
 cp ${location}/supervisor_conf/template.conf ${location}/supervisor_conf/supervisord.conf
-sed -i "s|172.17.0.2:5050|${MIP}:5050|" ${location}/supervisor_conf/supervisord.conf
-S1=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh)
-S2=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh)
-S3=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh)
+sed -i "s|172.17.0.2|${MIP}|" ${location}/supervisor_conf/supervisord.conf
+S1=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh-consul)
+S2=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh-consul)
+S3=$(docker run -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 mesos-slave-ssh-consul)
 S1IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S1})
 S2IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S2})
 S3IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S3})
