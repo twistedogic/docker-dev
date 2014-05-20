c919ab890fd8ae9fc8740ac2954ee46f06059ea1
vagrant <vagrant@dockernetwork>
2014-05-20 10:56:11 +0000

    add service

---
+++ services/services.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+docker pull sequenceiq/sonar-server
+docker pull sequenceiq/sonar-mysql
+docker pull crccheck/openrefine
+docker pull lukecyca/graylog2-docker
+docker pull aespinosa/jenkins
