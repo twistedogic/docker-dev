315912867f7ff0053200044defebfc6759f850da
vagrant <vagrant@precise64.(none)>
2014-05-21 15:55:39 +0000

    add log-management

---
+++ log-management/graylog2-docker/README.md
@@ -0,0 +1,10 @@
+graylog2-docker
+===============
+
+Debian-based, self-contained graylog2 image.
+
+
+
+## Using It
+
+The username/password is admin/admin.
--- services/services.sh
+++ services/services.sh
@@ -4,3 +4,4 @@ docker pull sequenceiq/sonar-mysql
 docker pull crccheck/openrefine
 docker pull lukecyca/graylog2-docker
 docker pull aespinosa/jenkins
+docker pull niallo/strider
