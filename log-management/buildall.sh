2e9ee8249fb1969dc8ee3a28afe9ca288787fa26
vagrant <vagrant@precise64.(none)>
2014-05-24 09:51:42 +0000

    finalize log-management

--- log-management/buildall.sh
+++ log-management/buildall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 location=$(pwd)
-echo "========graylog2========="
-cd ${location}/graylog2 && docker build -t graylog2 .
 echo "========logstash========="
 cd ${location}/logstash && docker build -t testnginx .
 echo "========kibana========="
 cd ${location}/kibana && docker build -t kibana .
-echo "========fluentd========="
-cd ${location}/fluentd && docker build -t fluentd .
 echo "build completed!"
--- log-management/fluentd/td-agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ES_HOST=${ES_HOST:-127.0.0.1}
-ES_PORT=${ES_PORT:-9300}
-sed -e "s/%%ES_HOST%%/$ES_HOST/" /etc/td-agent/td-agent.conf.tmp > /etc/td-agent/td-agent.conf
-/etc/init.d/td-agent restart
