caeb7ef929364c3d8464fdc5efebc371a2f3e78c
vagrant <vagrant@dockernetwork>
2014-05-27 10:44:48 +0000

    test usergrid

--- mesos-cluster/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-location=$(pwd)
-cd ${location}/docker-mesos/mesos-master-ssh && docker build -t mesos-master-ssh-consul .
-cd ${location}/docker-mesos/mesos-slave-ssh && docker build -t mesos-slave-ssh-consul .
-cd ${location}/master-with-marathon && docker build -t mesos-consul .
-echo "build completed!"
--- service-discovery/buildall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-location=$(pwd)
-cd ${location}/worker && docker build -t worker .
-cd ${location}/consul && docker build -t consul .
-cd ${location}/doozer && docker build -t doozer .
-cd ${location}/etcd && docker build -t etcd-ssh .
-echo "build completed!"
