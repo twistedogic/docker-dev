57dee6013b509b7db7432391d19d68a5413099cf
vagrant <vagrant@dockernetwork>
2014-05-28 07:39:20 +0000

    update consul

--- discovery/consul/consul-cluster.sh
+++ discovery/consul/consul-cluster.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 location=$(pwd)
-CID=$(docker run --name consul1 -m=104857600 -d -p 3000:3000 -p 8080:8080 -p 9002:22 -p 8500:8500 consul)
+KID=$(docker run --name kibana -h kibana -p 80:80 -p 9300:9300 -p 9200:9200 kibana)
+KIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${KID})
+CID=$(docker run --name consul1 -h master -e ES_HOST=${KIP} -d -p 3000:3000 -p 8080:8080 -p 9002:22 -p 8500:8500 consul)
 MIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID})
 cp ${location}/supervisor_conf/template.conf ${location}/supervisor_conf/supervisord.conf
 sed -i "s|172.17.0.2|${MIP}|" ${location}/supervisor_conf/supervisord.conf
-S1=$(docker run --name consul2 -d -p 7777:7777 -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 consul)
-S2=$(docker run --name consul3 -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 consul)
-S3=$(docker run --name consul4 -d -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ -m=104857600 consul)
+S1=$(docker run --name consul2 -d -h node01 -e ES_HOST=${KIP} -p 7777:7777 -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ consul)
+S2=$(docker run --name consul3 -d -h node02 -e ES_HOST=${KIP} -v ${location}/supervisor_conf:/etc/supervisor/conf.d/ consul)
 S1IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S1})
 S2IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S2})
-S3IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${S3})
 echo ${MIP}
 echo ${S1IP}
 echo ${S2IP}
-echo ${S3IP}
 docker ps -a
---
+++ discovery/consul/logstash.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+HOST=$(hostname)
+CFG=${CFG:-}
+ES_HOST=${ES_HOST:-127.0.0.1}
+ES_PORT=${ES_PORT:-9300}
+EMBEDDED="false"
+
+if [ "$ES_HOST" = "127.0.0.1" ] ; then
+    EMBEDDED="true"
+fi
+
+if [ "$CFG" != "" ]; then
+    wget $CFG -O /opt/logstash.conf --no-check-certificate
+else
+    cat << EOF > /opt/logstash.conf
+input {
+ file {
+ type => "${HOST}_log"
+ path => ["/var/log/supervisor/**"]
+ exclude => ["*.gz", "error.*"]
+ discover_interval => 10
+ }
+}
+output {
+  stdout { debug => true debug_format => "json"}
+EOF
+    if [ "$EMBEDDED" = "true" ]; then
+        cat << EOF >> /opt/logstash.conf
+  elasticsearch { embedded => $EMBEDDED }
+}
+EOF
+    else
+        cat << EOF >> /opt/logstash.conf
+  elasticsearch { embedded => $EMBEDDED host => "$ES_HOST" port => $ES_PORT }
+}
+EOF
+   fi
+fi
+
+java -jar /opt/logstash.jar agent -f /opt/logstash.conf -- web --backend elasticsearch://$ES_HOST:$ES_PORT/
