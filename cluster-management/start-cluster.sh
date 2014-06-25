c958afab427e1a95afe29b7273dd81033fcdc6a6
vagrant <vagrant@precise64.(none)>
2014-06-25 14:59:37 +0000

    setup storm

--- cluster-management/start-cluster.sh
+++ cluster-management/start-cluster.sh
@@ -7,5 +7,7 @@ sed -i "s|CHANGEME|${MIP}|" ${location}/slave_conf/supervisord.conf
 S1=$(docker run -h slave1 --name mesos-slave1 -p 5051:5051 --dns=172.17.42.1 --dns=8.8.8.8 -d -v ${location}/slave_conf:/etc/supervisor/conf.d/ -m=104857600 twistedogic/mesos-slave)
 S2=$(docker run -h marathon1 -p 8080:8080 --name marathon1 --dns=172.17.42.1 --dns=8.8.8.8 -d twistedogic/marathon /opt/marathon/bin/start --master zk://${MIP}:2181/mesos --zk zk://${MIP}:2181/marathon)
 S3=$(docker run -h chronos1 -p 8082:8081 --name chronos1 --dns=172.17.42.1 --dns=8.8.8.8 -d twistedogic/chronos /opt/chronos/bin/start-chronos.bash --master zk://${MIP}:2181/mesos --zk_hosts zk://${MIP}:2181/mesos --http_port 8081)
-#S3=$(docker run -d -p 8083:8080 -v ${location}/storm_conf/:/opt/storm/conf/ twistedogic/storm)
+cp ${location}/template.yaml ${location}/storm_conf/storm.yaml
+sed -i "s|CHANGEME|${MIP}|" ${location}/storm_conf/storm.yaml
+S4=$(docker run -d -p 8083:8080 -v ${location}/storm_conf/:/opt/storm/conf/ twistedogic/storm)
 docker ps -a
