9f8f63db6fff763b831361a098097ed985944f7e
root <root@compute03.cc.local>
2014-06-26 16:24:24 +0800

    update h2o

--- services/h2o/setup.r
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/Rscript
-if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
-if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-install.packages("h2o", repos=(c("http://s3.amazonaws.com/h2o-release/h2o/rel-knuth/11/R", getOption("repos"))))
