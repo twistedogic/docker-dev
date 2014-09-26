a36faaee1e7211e1ca34d822337f1fc229ec61ed
root <twistedogic@gmail.com>
2014-09-27 00:08:06 +0800

    update quickstart

--- services/quickstart.sh
+++ services/quickstart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 location=$(pwd)
+docker pull dockerfile/redis
+docker pull sequenceiq/hadoop-docker:2.5.1
 cd ${location}/worker && docker build -t twistedogic/worker .
 cd ${location}/h2o && docker build -t twistedogic/h2o .
 cd ${location}/r && docker build -t twistedogic/r .
+cd ${location}/openscoring && docker build -t twistedogic/openscoring .
