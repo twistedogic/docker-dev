a0549218aa44c22e5cda79f60f436cdafcd293b1
CoreOS Admin <twistedogic@gmail.com>
2014-11-09 15:34:09 +0000

    add monitoring

---
+++ monitoring/log.sh
@@ -0,0 +1,2 @@
+docker run -d -p 9300:9300 -p 9200:9200 -p 514:514 -p 80:80 twistedogic/kibana
+docker run -d -p 8000:8000 -v=/var/run/docker.sock:/tmp/docker.sock progrium/logspout syslog://10.1.42.1:514
