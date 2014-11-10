e1b0c891569150ee8559d889d116db435b6fe2c9
root <twistedogic@gmail.com>
2014-11-10 14:52:39 +0800

    update monitoring

--- monitoring/log.sh
+++ monitoring/log.sh
@@ -1,2 +1,3 @@
-docker run -d -p 9300:9300 -p 9200:9200 -p 514:514 -p 80:80 twistedogic/kibana
-docker run -d -p 8000:8000 -v=/var/run/docker.sock:/tmp/docker.sock progrium/logspout syslog://10.1.42.1:514
+KID=$(docker run -d -p 9300:9300 -p 9200:9200 -p 514:514 -p 80:80 twistedogic/kibana)
+KIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${KID})
+docker run -d -p 8000:8000 -v=/var/run/docker.sock:/tmp/docker.sock progrium/logspout syslog://${KIP}:514
--- monitoring/metric.sh
+++ monitoring/metric.sh
@@ -1,27 +1,29 @@
-docker run --name influxdb \
+DID=$(docker run --name influxdb \
   -d \
   -p 8083:8083 \
   -p 8086:8086 \
   --expose 8090 \
   --expose 8099 \
   -e PRE_CREATE_DB="cadvisor"  \
-  tutum/influxdb
+  tutum/influxdb)
+DIP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${DID})
 
 docker run --name cadvisor \
   -d \
+  -v /:/rootfs:ro \
   -v /var/run:/var/run:rw \
   -v /sys:/sys:ro \
   -v /var/lib/docker/:/var/lib/docker:ro \
   -p 8080:8080 \
   google/cadvisor:latest \
   --storage_driver=influxdb \
-  --storage_driver_host=10.1.42.1:8086 \
+  --storage_driver_host=${DIP}:8086 \
   --log_dir=/
 
 docker run --name grafana \
   -d \
   -p 8088:80 \
-  -e INFLUXDB_HOST=10.1.42.1 \
+  -e INFLUXDB_HOST=192.168.100.74 \
   -e INFLUXDB_PORT=8086 \
   -e INFLUXDB_NAME=cadvisor \
   -e INFLUXDB_USER=root \
