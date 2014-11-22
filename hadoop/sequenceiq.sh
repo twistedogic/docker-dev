9c92f0b0608bbf142c20c8dc6752de51e25a926c
root <twistedogic@gmail.com>
2014-11-23 00:27:29 +0800

    add restcommander

---
+++ hadoop/sequenceiq.sh
@@ -0,0 +1,6 @@
+docker pull sequenceiq/drill
+docker pull sequenceiq/kylin
+docker pull sequenceiq/spark-native-yarn
+docker pull sequenceiq/spark
+docker pull sequenceiq/hadoop-docker
+docker pull sequenceiq/ambari
---
+++ services/restcommander/run.sh
@@ -0,0 +1,1 @@
+python /opt/restcommander-master/play-1.2.4/play run /opt/restcommander-master/AgentMaster
