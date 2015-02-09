4bcde1af09bc3de95b2621115bdd5c4b7dc1cc46
root <twistedogic@gmail.com>
2015-02-09 08:43:46 +0000

    update

--- systemd/stop.sh
+++ systemd/stop.sh
@@ -1,8 +1,10 @@
 docker stop chronos
+docker stop marathon
 docker stop mesos_slave
 docker stop mesos_master
 docker stop zookeeper
 docker rm -v chronos
+docker rm -v marathon
 docker rm -v mesos_slave
 docker rm -v mesos_master
 docker rm -v zookeeper
