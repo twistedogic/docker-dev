058a081f66843459dbf40309a5cdb191ad274aaa
root <twistedogic@gmail.com>
2015-05-14 00:37:46 -0400

    update hdfs no serf

--- bigdata/hadoop/hdfs-namenode/docker-entrypoint.sh
+++ bigdata/hadoop/hdfs-namenode/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 if [ ! -f ${HDFS_NAMENODE_ROOT_DIR}/current/VERSION ]; then
 	echo Formatting namenode root fs in ${HDFS_NN_ROOT_DIR}
-	sed -i s/__MASTER__/$(hostname)/ /etc/hadoop/etc/hadoop/core-site.xml
 	bin/hdfs namenode -format
 fi
 
