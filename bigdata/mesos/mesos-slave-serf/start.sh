62a045aa4d8c1128a6572afd2b8785a2566f6eb5
root <twistedogic@gmail.com>
2015-05-19 01:54:39 -0400

    update mesos-spark

--- bigdata/mesos/mesos-slave-serf/start.sh
+++ bigdata/mesos/mesos-slave-serf/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
-mesos-master --ip=$IP \
-    --zk=zk://$ZK_IP:2181/mesos \
-    --work_dir=/var/lib/mesos/master \
-    --quorum=1
+mesos-slave --ip=$IP \
+    --master=zk://$ZK_IP:2181/mesos \
+    --work_dir=/var/lib/mesos/slave \
+    --log_dir=/var/log/mesos/slave
