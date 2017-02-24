bb34c0550ed4c5c0ca845461d068c8e8ee707b19
root <twistedogic@gmail.com>
2017-02-24 17:49:16 +0000

    add corenlp and remove openstack

--- openstack/keystone/setconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-crudini --set /etc/keystone/keystone.conf DEFAULT admin_token ADMIN
-crudini --set /etc/keystone/keystone.conf database connection mysql://keystone:Root123#@${MYSQL_HOST}/keystone
-crudini --set /etc/keystone/keystone.conf DEFAULT verbose True
-crudini --set /etc/keystone/keystone.conf token provider keystone.token.providers.uuid.Provider
-crudini --set /etc/keystone/keystone.conf token driver keystone.token.persistence.backends.sql.Token
-rm -f /var/lib/keystone/keystone.db
